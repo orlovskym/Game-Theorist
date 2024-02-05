f4a078ab0c0102a3f5c80954ca5e9c7e774e9158
Mark Orlovsky <orlovskym@gmail.com>
2024-02-04 22:34:57 -0800

    refactor to move game matrices to a separate file

--- game.test.ts
+++ game.test.ts
@@ -1,33 +1,23 @@
 import { describe, expect, it } from "vitest";
-import { Game, GameMatrix } from "./game";
-
-const PD_TEST: GameMatrix = [
-  [
-    [-1, -1],
-    [-12, 0],
-  ],
-  [
-    [0, -12],
-    [-8, -8],
-  ],
-];
+import { Game } from "./game";
+import { PrisonersDilemma } from "./games";
 
 describe("game", () => {
   it("will count its number of rows", () => {
     //arrange
-    const game = new Game(PD_TEST);
+    const game = new Game(PrisonersDilemma);
     //act
     const result = game.getNumberRows();
     //assert
     expect(result).toEqual(2);
   });
   it("will count its number of columns", () => {
-    const game = new Game(PD_TEST);
+    const game = new Game(PrisonersDilemma);
     const result = game.getNumberColumns();
     expect(result).toEqual(2);
   });
   it("will get a row by index", () => {
-    const game = new Game(PD_TEST);
+    const game = new Game(PrisonersDilemma);
     const result = game.getRowByIndex(0);
     expect(result).toEqual([
       [-1, -1],
@@ -35,7 +25,7 @@ describe("game", () => {
     ]);
   });
   it("will get a column by index", () => {
-    const game = new Game(PD_TEST);
+    const game = new Game(PrisonersDilemma);
     const result = game.getColumnByIndex(0);
     expect(result).toEqual([
       [-1, -1],
@@ -43,7 +33,7 @@ describe("game", () => {
     ]);
   });
   it("will remove a row by index", () => {
-    const game = new Game(PD_TEST);
+    const game = new Game(PrisonersDilemma);
     game.removeRowByIndex(0);
     expect(game.matrix).toEqual([
       [
@@ -53,7 +43,7 @@ describe("game", () => {
     ]);
   });
   it("will remove a column by index", () => {
-    const game = new Game(PD_TEST);
+    const game = new Game(PrisonersDilemma);
     game.removeColumnByIndex(1);
     expect(game.matrix).toEqual([[[-1, -1]], [[0, -12]]]);
   });
